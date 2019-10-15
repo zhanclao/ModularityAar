35280ae2d28456947af249020aed6b051966966d
laozhancheng <laozhancheng@szbit.cn>
2019-10-15 14:52:30 +0800

    add test scenario，edited AndroidManifest.

--- secondmodule/src/main/java/com/example/secondmodule/SecondModuleMainActivity.java
+++ secondmodule/src/main/java/com/example/secondmodule/SecondModuleMainActivity.java
@@ -1,15 +1,28 @@
 package com.example.secondmodule;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class SecondModuleMainActivity extends AppCompatActivity {
+import com.example.firstmodule.FirstModuleMainActivity;
+
+public class SecondModuleMainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity_main);
+
+        findViewById(R.id.btn_navigation_to_first_module).setOnClickListener(this);
     }
 
+
+    @Override
+    public void onClick(View view) {
+        if (R.id.btn_navigation_to_first_module == view.getId()) {
+            startActivity(new Intent(SecondModuleMainActivity.this, FirstModuleMainActivity.class));
+        }
+    }
 }
